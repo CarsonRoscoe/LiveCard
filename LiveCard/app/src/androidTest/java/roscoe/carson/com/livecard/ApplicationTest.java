35ee4e445eb78b875358335f42e86e4f75c9e6d0
CarsonRoscoe <carsonroscoe7@gmail.com>
2016-10-25 14:05:25 -0700

    Initial commit for Android app and server folder

---
+++ LiveCard/app/src/androidTest/java/roscoe/carson/com/livecard/ApplicationTest.java
@@ -0,0 +1,13 @@
+package roscoe.carson.com.livecard;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
